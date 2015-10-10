1f6ca70f8e9c98a9580591b4579913310c29f437
Zhu Ming <mingzhu.z+github@gmail.com>
2015-10-11 00:32:15 +0800

    method not used for now

--- common/items.js
+++ common/items.js
@@ -1,9 +1,9 @@
 Items = new Mongo.Collection("items");
 
-Meteor.methods({
-	getItem: function () {
-		return Items.find({}, {sort: {createdAt: 1}, limit: 1}).fetch();
-		// return Items.find({});
-		// console.info('server getFirstItem() ', this.item);
-	}
-});
+// Meteor.methods({
+// 	getItem: function () {
+// 		return Items.find({}, {sort: {createdAt: 1}, limit: 1}).fetch();
+// 		// return Items.find({});
+// 		// console.info('server getFirstItem() ', this.item);
+// 	}
+// });
