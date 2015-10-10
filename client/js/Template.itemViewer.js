ef8ef90fd9a50957473304148b79495b82971728
Zhu Ming <mingzhu.z+github@gmail.com>
2015-10-11 02:57:38 +0800

    skip to the next item

--- client/js/Template.itemViewer.js
+++ client/js/Template.itemViewer.js
@@ -1,22 +1,39 @@
 Template.itemViewer.created = function(){
+	this.currentItem = new ReactiveVar(null);
 	// this.item = Meteor.call("getItem");
 	// TODO: should subscribe to the data here
 }
 
 Template.itemViewer.helpers({
 	getItem: function() {
-		var item = Items.findOne();
-	  	console.info('helper getItem(): found item ', item);
-	  	return item;
+		var params = Router.current().params,
+			item;
+		// console.info('helper getItem(): params = ', params);
+		if (params && params._id) {
+			item = Items.findOne(params._id);
+		} else {
+			item = Items.findOne();
+		}
+		// console.info('helper getItem(): found item ', item);
+		Template.instance().currentItem.set(item);
+		return item;
 	}
 });
 
 Template.itemViewer.events({
-	'click .details': function(e, tmpl){
+	'click .details': function(e, tmpl) {
 		// show the current item in details
 		Router.go('itemDetails', {_id: this._id});
 	},
-	'click .skip': function(e, tmpl){
+	'click .skip': function(e, tmpl) {
 		// get next item
+		var item = tmpl.currentItem.get(),
+			next = Items.find(
+				{createdAt: {$gt: item.createdAt}}, 
+				{sort: {createdAt: 1}, limit: 1}
+			).fetch();
+		if (next && next[0] && next[0]._id) {
+			Router.go('item', {_id: next[0]._id});
+		}
 	}
 });
--- server/fixtures/ItemFixture.js
+++ server/fixtures/ItemFixture.js
@@ -4,12 +4,15 @@ Meteor.startup(function(){
 			image: '/images/apple-watch.jpg',
 			brief: "I really want to own this watch in 3 weeks!",
 			price: 16000,
-			story: "I tried on the Apple Watch Vintage in my cousin’s party last night and suddenly I felt like a movie star. People were telling me it’s a cool watch and I really wish it’s mine. The only problem? It costs 16 grand. My cousin had been working all along to afford this watch and I think I can too."
-		},{
+			story: "I tried on the Apple Watch Vintage in my cousin’s party last night and suddenly I felt like a movie star. People were telling me it’s a cool watch and I really wish it’s mine. The only problem? It costs 16 grand. My cousin had been working all along to afford this watch and I think I can too.",
+			createdAt: new Date(2015, 10, 11, 1, 0, 0, 0)
+		});
+		Items.insert({
 			image: '/images/david-beckham.jpg',
 			brief: "I really want to own this guy in 2016",
 			price: 1000000,
-			story: "Damn my youth is running out! I need a husband NOW! Can I buy this man?!"
+			story: "Damn my youth is running out! I need a husband NOW! Can I buy this man?!",
+			createdAt: new Date(2015, 10, 11, 2, 0, 0, 0)
 		});
 	}
 });
