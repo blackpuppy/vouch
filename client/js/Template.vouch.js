9a13d2efea499e66e25f0af55dea464062322d86
Zhu Ming <mingzhu.z+github@gmail.com>
2015-10-11 10:33:27 +0800

    add vouch template

---
+++ client/js/Template.vouch.js
@@ -0,0 +1,20 @@
+Template.itemDetails.helpers({
+	getItem: function() {
+		var params = Router.current().params;
+		var item = Items.findOne(params._id);
+		// console.info('helper getItem(): found item ', item);
+		return item;
+	}
+});
+
+Template.itemDetails.events({
+	'click .upload': function(e, tmpl){
+		// upload a photo
+	},
+	'click .takePhoto': function(e, tmpl){
+		// take a photo with camera
+	},
+	'click .search': function(e, tmpl){
+		// search a photo
+	}
+});
